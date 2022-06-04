70736276035a9226cdd9e470ee495abd44f416e3
Nityananda Lorenzo Hernández <nityanandalhn@gmail.com>
2022-06-04 19:10:27 +0200

    PROBLEMA DE RENDIMIENTO

--- Criptoanalisis/Datos/Mappers/EndpointMapper.cs
+++ Criptoanalisis/Datos/Mappers/EndpointMapper.cs
@@ -10,7 +10,9 @@ namespace Datos.Mappers
             Id = endpoint.Id,
             Tipo = endpoint.Tipo,
             Url = endpoint.Url,
-            Parametros = endpoint.ParametrosEndpoints!.Select(x => x.Parametros!.Valor).ToList()!
+            Parametros = endpoint.ParametrosEndpoints!.Select(x => x.Parametros!.Valor).ToList()! //Problema de rendimiento
+            //Acabo de darme cuenta que llamar a ToList hace que entity framework lance las query.
+            //Pendiente de optimizar
         };
 
         public static Endpoints FromDto(EndpointDto dto) => new()
