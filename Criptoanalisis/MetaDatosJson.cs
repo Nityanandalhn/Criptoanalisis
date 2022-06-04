4222ebb46d1b628e46a6193eb49f1c2762462a45
Nityananda Lorenzo Hernández <nityanandalhn@gmail.com>
2022-06-04 11:13:10 +0200

    limpieza

--- Criptoanalisis/MetaDatosJson.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Criptoanalisis
-{
-    public class MetaDatosJson : Attribute
-    {
-        private string clave;
-
-        public MetaDatosJson(string clave) => this.clave = clave;
-    }
-}
--- Criptoanalisis/Program.cs
+++ Criptoanalisis/Program.cs
@@ -6,9 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-/// <summary>
-/// Preparo mi servicio para poder inyectar sus implementaciones.
-/// </summary>
+
 builder.Services.AddScoped(typeof(IRepoBase<,>), typeof(RepoBaseImpl<,>));
 builder.Services.AddScoped(typeof(EndpointRepository));
 builder.Services.AddControllers().AddJsonOptions(x => x.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.IgnoreCycles);
